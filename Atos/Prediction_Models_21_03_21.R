2988fe373b6ba67d24674d75743c7026ae2c244f
Kamran Afzali <kamranafzali@Kamrans-MBP.lan>
2021-03-24 10:43:08 -0400

    Atos1

--- Atos/Prediction_Models_21_03_21.R
+++ Atos/Prediction_Models_21_03_21.R
@@ -2,6 +2,7 @@ setwd(paste(dirname(rstudioapi::getActiveDocumentContext()$path),"",sep = ""))
 load("~/OneDrive - Universite de Montreal/Usydney/data.RData")
 library(haven)
 library(tidyverse)
+library(tidymodels)
 
 #data_Atos <- read_sav("HU OD 1 5 10y model.sav")
 #View(data_Atos)
@@ -18,6 +19,26 @@ Data_merged=DATA %>% inner_join(data_Atos, by = "id0101")
 
 #check the pred input with new Excel file
 ##good for now but have to check later!
+table(Data_merged$HU_1YR)
+out="HU_1YR"
+
+df=Data_merged%>%
+  select(c(out,preds))
+
+
+df_split <- initial_split(df)
+train_data <- training(df_split)
+test_data <- testing(df_split)
+cv_train <- vfold_cv(train_data, v = 10, repeats = 5, strata = out)
+
+rec_obj <- recipe(HU_1YR ~ ., data = df)
+imputed <- rec_obj %>%
+  step_knnimpute(all_predictors()) 
+ind_vars <- imputed %>%
+  step_dummy(all_predictors(), -all_numeric()) 
+standardized <- ind_vars %>%
+  step_center(all_predictors())  %>%
+  step_scale(all_predictors()) 
 
 #SVM
 
