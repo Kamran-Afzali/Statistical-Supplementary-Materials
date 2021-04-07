f8212b827f3c5cf35010109296178944d68e9b0f
Kamran Afzali <kamranafzali@Kamrans-MBP.lan>
2021-04-07 09:52:48 -0400

    hour8

--- Atos/Prediction_Models_21_03_21.R
+++ Atos/Prediction_Models_21_03_21.R
@@ -12,8 +12,13 @@ library(plotly)
 
 #data_Atos <- read_sav("HU OD 1 5 10y model.sav")
 #View(data_Atos)
-
+colnames( data_Atos)
 Out_Comes=c("STA_1YR","STA_5YR","MTA_5YR","STA_10YR","MTA_10YR","LTA_10yr","HU_1YR","HU_5YR","HU_10YR","OD_upto1YR","OD_upto5YR","OD_upto10YR","DEATH_1YR","DEATH_5YR","DEATH_10YR","DEATH_15YR")
+
+Out_Comes[!(Out_Comes%in%colnames( data_Atos))]
+Out_Comes[(Out_Comes%in%colnames( data_Atos))]
+
+preds
 #colnames(data_Atos)%in%preds
 
 #DATA$id0101=as.numeric(as.character(DATA$id0101))
